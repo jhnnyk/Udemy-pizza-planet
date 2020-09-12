5c71e8ceefd79f75cf77f2a5abe09042f9ba21de
John Knotts <jhnnyk@gmail.com>
2020-09-12 14:04:42 -0600

    home page component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
   <div id="app">
     <app-header />
+    <home />
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Home from './components/Home';
 
 export default {
   name: 'App',
   components: {
     appHeader: Header,
+    Home,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
