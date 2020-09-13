ab4928a3421e86a1133acee6426fce8eb05793ac
John Knotts <jhnnyk@gmail.com>
2020-09-13 11:04:11 -0600

    form input bindings

--- src/components/NewPizza.vue
+++ src/components/NewPizza.vue
@@ -3,41 +3,70 @@
     <h3>Add new pizza:</h3>
     <div class="form-group">
       <label for="name">Name</label>
-      <input type="text" id="name" />
+      <input type="text" id="name" v-model="newPizza.name" />
     </div>
     <div class="form-group">
       <label for="description">Description</label>
-      <textarea id="description"></textarea>
+      <textarea
+        id="description"
+        v-model="newPizza.description"
+        rows="5"
+      ></textarea>
     </div>
     <p>
       <strong>Option 1:</strong>
     </p>
     <div class="form-group">
       <label for="size1">Size</label>
-      <input type="text" id="size1" />
+      <input type="text" id="size1" v-model="newPizza.options[0].size" />
     </div>
     <div class="form-group">
       <label for="price1">Price</label>
-      <input type="text" id="price1" />
+      <input type="text" id="price1" v-model="newPizza.options[0].price" />
     </div>
     <p>
       <strong>Option 2:</strong>
     </p>
     <div class="form-group">
       <label for="size2">Size</label>
-      <input type="text" id="size2" />
+      <input type="text" id="size2" v-model="newPizza.options[1].size" />
     </div>
     <div class="form-group">
       <label for="price2">Price</label>
-      <input type="text" id="price2" />
+      <input type="text" id="price2" v-model="newPizza.options[1].price" />
     </div>
-    <button type="button" class="btn_green">Add</button>
+    <button type="button" class="btn_green" @click="add">Add</button>
+    {{ this.newPizza }}
   </form>
 </template>
 
 <script>
 export default {
   name: 'addNewPizza',
+  data() {
+    return {
+      newPizza: {
+        name: 'Eg. Ham and Pineapple',
+        description:
+          'Eg. A delicious tomato based pizza topped with mozzarella, ham and pineapple',
+        options: [
+          {
+            size: 9,
+            price: 7.95,
+          },
+          {
+            size: 12,
+            price: 12.95,
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    add() {
+      alert('method triggered');
+    },
+  },
 };
 </script>
 
