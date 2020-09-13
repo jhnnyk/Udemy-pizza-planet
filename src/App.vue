b8c8cf3c167af9adec43b26a94a7b364463b547f
John Knotts <jhnnyk@gmail.com>
2020-09-13 10:23:03 -0600

    admin component

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,23 @@
   <div id="app">
     <appHeader />
     <Home />
-    <Menu />
+    <Admin />
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
 import Home from './components/Home';
-import Menu from './components/Menu';
+// import Menu from './components/Menu';
+import Admin from './components/Admin';
 
 export default {
   name: 'App',
   components: {
     appHeader: Header,
     Home,
-    Menu,
+    // Menu,
+    Admin,
   },
 };
 </script>
