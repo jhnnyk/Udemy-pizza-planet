cb02c034ab6d9501bb2a7a59672dd77bc33397a4
John Knotts <jhnnyk@gmail.com>
2020-09-14 10:51:16 -0600

    refactor routes

--- src/main.js
+++ src/main.js
@@ -1,56 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
-import Home from './components/Home';
-import Menu from './components/Menu';
-import Admin from './components/Admin';
-import About from './components/About';
-import Contact from './components/Contact';
-import History from './components/History';
-import Delivery from './components/Delivery';
-import OrderingGuide from './components/OrderingGuide';
+import { routes } from './routes';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
-const routes = [
-  { path: '/', name: 'homeLink', component: Home },
-  { path: '/menu', name: 'menuLink', component: Menu },
-  { path: '/contact', name: 'contactLink', component: Contact },
-  {
-    path: '/admin',
-    name: 'adminLink',
-    component: Admin,
-    beforeEnter: (to, from, next) => {
-      alert('this area is for authorized users only, please login to continue');
-      next();
-    },
-  },
-  {
-    path: '/about',
-    component: About,
-    name: 'aboutLink',
-    children: [
-      { path: '/history', name: 'historyLink', component: History },
-      { path: '/delivery', name: 'deliveryLink', component: Delivery },
-      {
-        path: '/ordering-guide',
-        name: 'orderingGuideLink',
-        component: OrderingGuide,
-      },
-    ],
-  },
-  { path: '*', redirect: '/' },
-];
-
 const router = new VueRouter({
   routes,
   mode: 'history',
-  scrollBehavior(to) {
-    if (to.hash) {
-      return { selector: to.hash };
-    }
-  },
 });
 
 new Vue({
