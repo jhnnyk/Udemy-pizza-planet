a3dbb34109ca3dc8fbd006e8e4d9deadc24c5683
John Knotts <jhnnyk@gmail.com>
2020-09-15 09:23:04 -0600

    sync orders with firebase

--- src/store/modules/orders.js
+++ src/store/modules/orders.js
@@ -1,16 +1,24 @@
+import { firestoreAction } from 'vuexfire';
+import { dbOrdersRef } from '../../firebase';
+
 const state = {
   orders: [],
 };
 
 const getters = {
   numberOfOrders: (state) => state.orders.length,
+  getOrders: (state) => state.orders,
 };
 
 const mutations = {
   addOrder: (state, order) => state.orders.push(order),
 };
 
-const actions = {};
+const actions = {
+  setOrdersRef: firestoreAction((context) => {
+    return context.bindFirestoreRef('orders', dbOrdersRef);
+  }),
+};
 
 export default {
   state,
